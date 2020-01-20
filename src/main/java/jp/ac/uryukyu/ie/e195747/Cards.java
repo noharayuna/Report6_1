17334fb369b79af9bfb1a68e2ec4ca6598087841
Yuna Nohara <e195747@ie.u-ryukyu.ac.jp>
2020-01-20 21:15:56 +0900

    Commit 2

--- src/main/java/jp/ac/uryukyu/ie/e195747/Cards.java
+++ src/main/java/jp/ac/uryukyu/ie/e195747/Cards.java
@@ -1,4 +1,18 @@
 package jp.ac.uryukyu.ie.e195747;
 
+import java.util.ArrayList;
+
 public class Cards {
+    void deck() {
+
+        ArrayList<String> deck = new ArrayList<>(53);
+        for (int i = 1; i < 14; i++) {
+            deck.add("スペード：" + i);
+            deck.add("ダイヤ：" + i);
+            deck.add("ハート：" + i);
+            deck.add("クラブ：" + i);
+        }
+        deck.add("ジョーカー");
+        //System.out.println(deck);
+    }
 }
