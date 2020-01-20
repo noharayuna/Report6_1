a199e81ac1265f3da00251adc5a9d8be6ef6ec27
Yuna Nohara <e195747@ie.u-ryukyu.ac.jp>
2020-01-20 21:29:22 +0900

    Commit 1

---
+++ src/test/java/jp/ac/uryukyu/ie/e195747/CardsTest.java
@@ -0,0 +1,12 @@
+package jp.ac.uryukyu.ie.e195747;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardsTest {
+
+    @Test
+    void deck() {
+    }
+}
